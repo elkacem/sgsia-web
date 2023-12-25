0cf6319e96982baa53345421900351a7d3dfa89d
elkacem <https://github.com/elkacem>
2023-12-25 14:44:02 +0100

    add auth and the endpoints

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Surveys;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -20,6 +21,10 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        User::factory()->create([
+            'name' => 'belo',
+            'email' => 'belo@mail.com', 
+        ]);
         Surveys::factory(40)->create();
     }
 }
