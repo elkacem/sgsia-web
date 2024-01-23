2dcd87b97d8a7c0b15bc3064e10a39cfa1f265c5
elkacem <belkacemyesse@gmail.com>
2024-01-23 15:25:23 +0100

    adding some custumisation

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => 'auth'] , function() {
 
-    Route::get('/linkstorage', function () {
-        Artisan::call('storage:link');
-    });
+    // Route::get('/linkstorage', function () {
+    //     Artisan::call('storage:link');
+    // });
 
     // $this->middleware
 
